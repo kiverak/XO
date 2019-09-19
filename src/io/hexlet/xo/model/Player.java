196c8b7dba03a2ee0207e388cd1e4c0c60e009f7
kiverak <kiverak@gmail.com>
2019-09-19 18:20:02 +0400

    Module5.Lesson5_Builder

--- src/io/hexlet/xo/model/Player.java
+++ src/io/hexlet/xo/model/Player.java
@@ -3,14 +3,10 @@ package io.hexlet.xo.model;
 public class Player {
     private final String name;
     private final Figure figure;
-    String address;
-    int score;
-    String nick;
-    String uuid;
 
-    public Player(String name, Figure figure) {
-        this.name = name;
-        this.figure = figure;
+    private Player(final PlayerBuilder playerBuilder) {
+        this.name = playerBuilder.getName();
+        this.figure = playerBuilder.getFigure();
     }
 
     public String getName() {
@@ -19,5 +15,33 @@ public class Player {
 
     public Figure getFigure() {
         return figure;
+    }
+
+    //Builder
+    public class PlayerBuilder {
+        private String name;
+        private Figure figure;
+
+        public playerBuilder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public playerBuilder figure(final Figure figure) {
+            this.figure = figure;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Figure getFigure() {
+            return figure;
+        }
+
+        public Player build() {
+            return new Player(this);
+        }
     }
 }
